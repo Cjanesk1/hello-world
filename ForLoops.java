c055f6f096e6af1d973ed24df2af43180e32ad90
Cjanesk1 <54916560+Cjanesk1@users.noreply.github.com>
2019-09-18 15:06:01 -0700

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,23 @@
+public class ForLoops
+{
+    public static void main ( String args[] ) {
+        //int cube, a;
+        //cube = 1;
+        // for (a=0; a <=10; a++) {
+            //cube = a*a*a
+            //System.out.println(a "/t" + cube);
+            
+        //int a;
+        
+        //for (a = 99; a>0; a=a-2) {
+            //System.out.println(a);
+        int n;
+        String b = "The quick brown fox jumped over the lazy brown dog";
+        n = b.length();
+        b.charAt(0);
+        for (int l = 0; l<b.length(); l++) {
+            System.out.println(b.charAt(l));
+                   
+    }
+} 
+}   
